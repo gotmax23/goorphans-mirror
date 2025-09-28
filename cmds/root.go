de8ce9ceb191a73772a65e4ef65b9c7abdba6b4c
Maxwell G <maxwell@gtmx.me>
2025-09-28 01:13:07 -0500

    Get rid of extra docgen dependencies

--- cmds/root.go
+++ cmds/root.go
@@ -2,12 +2,10 @@ package cmds
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 	"go.gtmx.me/goorphans/config"
 	"go.gtmx.me/goorphans/fasjson"
 )
@@ -104,28 +102,28 @@ func NewRootCmd() *cobra.Command {
 	rootCmd.AddCommand(newOrphansCommand())
 	rootCmd.AddCommand(newFas2emailCommand())
 	rootCmd.AddCommand(NewDistgitCmd())
-	rootCmd.AddCommand(newDocsGenCmd())
+	// rootCmd.AddCommand(newDocsGenCmd())
 	return rootCmd
 }
 
 // TODO: Playing around with docs gen
-func newDocsGenCmd() *cobra.Command {
-	out := "docs"
-	cmd := &cobra.Command{
-		Use:    "_docs",
-		Hidden: true,
-		RunE: func(cmd *cobra.Command, argv []string) error {
-			if err := os.Mkdir(out, 0o755); err != nil && !errors.Is(err, os.ErrExist) {
-				return err
-			}
-			root := NewRootCmd()
-			root.DisableAutoGenTag = true
-			return doc.GenMarkdownTree(root, out)
-		},
-	}
-	cmd.Flags().StringVarP(&out, "out", "o", out, "")
-	return cmd
-}
+// func newDocsGenCmd() *cobra.Command {
+// 	out := "docs"
+// 	cmd := &cobra.Command{
+// 		Use:    "_docs",
+// 		Hidden: true,
+// 		RunE: func(cmd *cobra.Command, argv []string) error {
+// 			if err := os.Mkdir(out, 0o755); err != nil && !errors.Is(err, os.ErrExist) {
+// 				return err
+// 			}
+// 			root := NewRootCmd()
+// 			root.DisableAutoGenTag = true
+// 			return doc.GenMarkdownTree(root, out)
+// 		},
+// 	}
+// 	cmd.Flags().StringVarP(&out, "out", "o", out, "")
+// 	return cmd
+// }
 
 func Execute() {
 	err := NewRootCmd().Execute()
--- go.mod
+++ go.mod
@@ -14,14 +14,8 @@ require (
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/kr/pretty v0.2.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	golang.org/x/sys v0.36.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
